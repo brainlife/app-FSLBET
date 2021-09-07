916c26ff630953a6c915baf18104da250b0cbecc
Brad Caron <bacaron@iu.edu>
2021-09-07 10:13:18 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,6 +8,7 @@ export bvals=$(jq -r .bvals config.json)
 export bvecs=$(jq -r .bvecs config.json)
 export fthresh=$(jq -r .fthreshold config.json)
 export gthresh=$(jq -r .gthreshold config.json)
+robust=`jq -r '.robust' config.json`
 
 cp ${dwi} dwi_original.nii.gz;
 cp ${bvals} ./dwi/dwi.bvals;
@@ -23,17 +24,20 @@ fslmaths nodif \
     -Tmean \
     nodif_mean;
 
-# Brain extraction before alignment
+# Brain extraction
+if [[ ${robust} == true ]]; then
+    r_line='-R '
+else
+    r_lin=' '
+fi
 bet nodif_mean.nii.gz \
     dwi_brain \
-    -R \
+    ${r_line}\
     -f $fthresh \
     -g $gthresh \
     -m;
     
-# multiply dwi by mask to get extracted brain
-fslmaths dwi_original.nii.gz -mul dwi_brain_mask.nii.gz ./dwi/dwi.nii.gz;
-
+mv dwi_brain.nii.gz ./dwi/dwi.nii.gz
 mv dwi_brain_mask.nii.gz ./mask/mask.nii.gz;
 rm -rf nodif.nii.gz nodif_mean.nii.gz dwi_original.nii.gz dwi_brain.nii.gz
 
